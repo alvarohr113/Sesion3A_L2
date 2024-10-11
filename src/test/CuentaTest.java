8e57cd7c8ad2a0af48f1537fea6472979e12b4f6
alvarohr113 <alvaro201102@gmail.com>
2024-10-11 10:28:10 +0200

    Primer commit CuentaTest

---
+++ src/test/CuentaTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+	
+	static Cuenta cuenta1;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		cuenta1 = new Cuenta(0);
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+
+		cuenta1.ingresar(1);
+		
+		assertEquals(1,cuenta1.getSaldo());
+		
+	}
+
+}
